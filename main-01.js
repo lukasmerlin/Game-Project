e071a26776047209d238b6d44fd626fed188eef9
Lukas Merlin Jürgens <108548830+lukasmerlin@users.noreply.github.com>
2023-01-24 15:50:36 +0100

    So broken

--- main-01.js
+++ main-01.js
@@ -78,32 +78,66 @@ function start() {
         }
         
         update() {
-            this.velocity.y += gravity;
+            //console.log(this.velocity.y);
+            //console.log(this.position.y);
+
             this.position.y += this.velocity.y;
             this.position.x += this.velocity.x;
-            console.log(this.velocity.x)
         
             // Update weapon position to match player position
             weapons[0].position.x = this.position.x;
             weapons[0].position.y = this.position.y;
     
-                // Check for collision with platforms
-            for (const platform of platforms) {
-                if (this.position.x + this.width >= platform.position.x && this.position.x <= platform.position.x + platform.width) {
-                    if (this.position.y + this.height >= platform.position.y && this.position.y <= platform.position.y + platform.height) {
-                         // Check if player is colliding from below
-                         if (this.velocity.y > 0) {
-                             // Adjust player's position to be on top of platform
-                             this.position.y = platform.position.y - this.height;
-                             this.velocity.y = 0;
-                             this.onGround = true;
-                         } else {
-                             // Player is jumping from below
-                             // Do nothing
-                         }
-                    }
-                 }
+            platforms.forEach(platform =>{
+                if (this.position.x < platform.position.x + platform.width && 
+                    this.position.x + this.width > platform.position.x && 
+                    this.position.y < platform.position.y + platform.height &&
+                    this.position.y + this.height > platform.position.y){
+                    console.log("platform collision")
+                    console.log(this.velocity.y)
+                        if(this.velocity.y > 0 && this.position.x + this.width > platform.position.x - 20){
+                            this.position.y = platform.position.y - this.height;
+                            this.velocity.y = 0;
+                            this.onGround = true;
+                            console.log("platform collision top")
+                        }
+                        else if(this.velocity.y < 0){
+                            console.log("platform collision bottom")
+                            this.position.y = platform.position.y + platform.height;
+                            this.velocity.y = 0;
+                        }
+                } 
+            } )     
+
+            if (!this.onGround){
+                this.velocity.y += gravity;      
             }
+
+/*
+            if (this.position.x + this.width >= platform.position.x && this.position.x <= platform.position.x + platform.width) {
+                if (this.position.y + this.height >= platform.position.y && this.position.y <= platform.position.y + platform.height) {
+                    // Check if player is colliding from below
+                    if (this.position.y + this.height <= platform.position.y + platform.height && this.position.y + this.height >= platform.position.y  && this.velocity.y > 0 ) {
+                        this.velocity.y = 0;
+                        this.onGround = true;
+                    }
+                    // Check if player is colliding from above
+                    if (this.position.y <= platform.position.y + platform.height && this.position.y >= platform.position.y && this.velocity.y < 0 ) {
+                        this.velocity.y = 0;
+                        this.position.y = platform.position.y + platform.height;
+                    }
+                    // Check if player is colliding from the side
+                    if(this.position.x + this.width >= platform.position.x && this.position.x + this.width <= platform.position.x + platform.width){
+                        this.velocity.x = 0;
+                    }
+                    if(this.position.x <= platform.position.x + platform.width && this.position.x >= platform.position.x){
+                        this.velocity.x = 0;
+                    }
+                }
+            }*/
+            
+                
+            
 
             // Check for collision with enemies
             for (const enemy of enemies) {
@@ -127,11 +161,15 @@ function start() {
 
         
             // Check for collision with floor
-            if (this.position.y + this.height >= floor.position.y) {
+            /*
+            if (this.position.y + this.height >= floor.position.y && this.position.x <= floor.position.x + floor.width) {
                 this.position.y = floor.position.y - this.height;
                 this.velocity.y = 0;
                 this.onGround = true;
-            }
+            }*/
+
+            // Check if player falls off the bottom of the screen
+            if (this.position.y > canvas.height) this.health = 0;
     
             // Check for collision with edges of canvas
             if (this.position.x <= 0) {
@@ -165,14 +203,28 @@ function start() {
                 // Trigger game over event
                 console.log("Game over!");
                 gameOver();
-                //restartGame();
             }
         
             this.draw();
             weapons[0].draw();
         }
-        
     }
+
+    class levelEnd {
+        constructor(x, y, width = 100, height = 200) {
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+            this.fillStyle = 'blue';
+        }
+
+        draw() {
+            c.fillStyle = this.fillStyle;
+            c.fillRect(this.x, this.y, this.width, this.height);
+        }
+    }
+
 
     class DeathHole {
         constructor(x, width) {
@@ -188,11 +240,6 @@ function start() {
             c.fillRect(this.x, this.y, this.width, this.height);
         }
     }
-    
-    const deathHoles = [
-        new DeathHole(700, 700,),
-        new DeathHole(3000, 500,)
-    ];
 
     class HealthBar {
         constructor(health){
@@ -209,10 +256,10 @@ function start() {
     }
  
     class Platform {
-        constructor({x, y}){
+        constructor({x, y}, width, height){
             this.position = {x, y};
-            this.width = 200;
-            this.height = 20;
+            this.width = width;
+            this.height = height;
         }  
         draw(){
             c.fillStyle = 'green';
@@ -221,12 +268,14 @@ function start() {
     }
     
     class enemy {
-        constructor({x, y, health = 100}){
+        constructor({x, y, health = 40}){
             this.position = {x, y};
             this.width = 150;
             this.height = 150;
             this.velocity = {x: 0, y: 0};
             this.health = health;
+            this.onGround = false;
+            this.lastChase = Date.now();
         }
         
         draw(){
@@ -240,11 +289,23 @@ function start() {
     
         chase(player) {
             // Calculate the direction to move towards the player
+            // Calculate the angle between the enemy and the player
             let xDiff = player.position.x - this.position.x;
-            let direction = xDiff / Math.abs(xDiff);
+            let yDiff = player.position.y - this.position.y;
+            let angle = Math.atan2(yDiff, xDiff);
+
+            // Set the velocity in the direction of the angle
+            this.velocity.x = Math.cos(angle) * 10;
+            this.velocity.y = Math.sin(angle) * 10;
+            
     
-            // Move the enemy towards the player on the x-axis
-            this.velocity.x = direction * 5;
+            // Check for collision with player
+            if (this.position.x + this.width >= player.position.x && this.position.x <= player.position.x + player.width) {
+                this.destroy();
+            }
+
+
+            
         }
     
         destroy() {
@@ -253,30 +314,19 @@ function start() {
     
     
         update() {
-            this.velocity.y += gravity;
+            // this.velocity.y += gravity;
             this.position.y += this.velocity.y;
             this.position.x += this.velocity.x;
             // this.chase(player);
         
-            // Check for collision with floor
-            if (this.position.y + this.height >= floor.position.y) {
-                this.position.y = floor.position.y - this.height;
-                this.velocity.y = 0;
-            }
         
             // Check for collision with edges of canvas
             if (this.position.x + this.width >= canvas.width || this.position.x <= 0) {
                 this.velocity.x =- this.velocity.x;
             }
     
-            // Check for collision with platforms
-            for (const platform of platforms) {
-                if (this.position.x + this.width >= platform.position.x && this.position.x <= platform.position.x + platform.width) {
-                    if (this.position.y + this.height >= platform.position.y && this.position.y <= platform.position.y + platform.height) {
-                        this.velocity.y = -this.velocity.y;
-                    }
-                }
-            }
+  
+
     
             // Check for collision with projectiles
             for (const projectile of projectiles) {
@@ -289,13 +339,19 @@ function start() {
                 }
             }
     
+            if(this.position.y + this.height >= canvas.height){
+                this.destroy();
+            }
     
             // Check for health <= 0
             if (this.health <= 0) {
                 this.destroy();
             }
         
-           // this.chase(player);
+           if(player.position.x + 1500 >= this.position.x ){
+            this.chase(player);
+           }
+
             this.draw();
         }
         
@@ -333,19 +389,12 @@ function start() {
         }
     }
     
-    class Floor extends Platform {
-        constructor({ y, height }) {
-            super({ x: 0, y });
-            this.height = height;
-            this.width = 6000;
-        }
-    }
-    
     const keys = {
         right: { pressed:false},
         left: { pressed:false},
     }
 
+// Press G to restart the Game
     window.addEventListener("keydown", event => {
         if (event.code === "KeyG") {
             start();
@@ -354,9 +403,13 @@ function start() {
     
 
     function gameOver(){
-        // stop the animation
-        // clear the canvas
-        scrollOffSet = 0;
+        /* player = [];  
+        enemies = [];
+        projectiles = [];
+        platforms = [];
+        weapons = []; 
+        */ 
+
         c.clearRect(0, 0, canvas.width, canvas.height);
         c.fillStyle = 'black';
         c.fillRect(0, 0, canvas.width, canvas.height);
@@ -365,21 +418,21 @@ function start() {
         c.fillText('GAME OVER', 100, 200);
         c.font = '50px Arial';
         c.fillText('Press G to restart', 100, 300);
-        // call start function when space is pressed
     };
+    
 
     
     function animate(){
         requestAnimationFrame(animate);
         c.clearRect(0, 0, canvas.width, canvas.height);
         backgroundLevel1.draw();
-        player.update();
         platforms.forEach(platform => {platform.draw();});
+        player.update();
         enemies.forEach(enemy => {enemy.update();});
         weapons.forEach(weapon => {weapon.draw();});
         deathHoles.forEach(deathHole => {deathHole.draw();});
-        console.log(player.health);
         healthbar.draw();
+        endLevel.draw();
     
         // Update projectiles
         projectiles.forEach(projectile => { projectile.update(); });
@@ -387,32 +440,76 @@ function start() {
         if (keys.right.pressed && player.position.x < 650) {player.velocity.x = 5;} 
         else if (keys.left.pressed && player.position.x > 100){player.velocity.x = -5;}
         else {  player.velocity.x = 0;
-                if (keys.right.pressed) {platforms.forEach(platform => {platform.position.x -= 5; scrollOffSet += 5; if (scrollOffSet > 5000) {alert('You Win');}}); for (const deathHole of deathHoles) {
-                    deathHole.x -= 5;
-                }}
-            
+                if (keys.right.pressed) {
+                    for (const platform of platforms) { platform.position.x -= 5;} 
+                    for (const deathHole of deathHoles) { deathHole.x -= 5;}
+                    for (const enemy of enemies) { enemy.position.x -= 5;}
+                }   
         }
+
+        if(player.position.x + player.width > levelEnd.x){
+            c.clearRect(0, 0, canvas.width, canvas.height);
+            c.fillStyle = 'black';
+            c.fillRect(0, 0, canvas.width, canvas.height);
+            c.fillStyle = 'red';
+            c.font = '100px Arial';
+            c.fillText('YOU WIN', 100, 200);
+            c.font = '50px Arial';
+            c.fillText('Press G to restart', 100, 300);
+        }
+
+
+
     
     // Platform collision detection
-        platforms.forEach(platform => {
-            if (player.position.y + player.height <= platform.position.y && 
-                player.position.y + player.height + player.velocity.y >= platform.position.y && 
-                player.position.x + player.width >= platform.position.x &&
-                player.position.x <= platform.position.x + platform.width){
-                player.velocity.y = 0;
-            }
-        })
+ 
     }
 
     const player = new Player({imageSrc: './img/megaman/idle-right.png' });
-    const platforms = [new Platform({x:100, y:900}), new Platform({x: 500, y: 800})];
-    let enemies = [new enemy({x: 1000, y: 200})];
+
+
+    const platforms = [
+        new Platform({x: 0,    y: canvas.height-100}, 1000, 200 ), 
+        new Platform({x: 1200, y: canvas.height-400}, 1000, 600 ),
+        new Platform({x: 2500, y: canvas.height-100}, 1000, 200 ),
+        new Platform({x: 3700, y: canvas.height-200}, 200, 20 ),
+        new Platform({x: 4000, y: canvas.height-300}, 200, 20 ),
+        new Platform({x: 4300, y: canvas.height-400}, 200, 20 ),
+        new Platform({x: 4600, y: canvas.height-250}, 200, 20 ), 
+        new Platform({x: 5100, y: canvas.height-100}, 500, 500 ),
+        new Platform({x: 5600, y: canvas.height-200}, 100, 500 ),
+        new Platform({x: 5700, y: canvas.height-300}, 100, 500),
+        new Platform({x: 5800, y: canvas.height-400}, 1000, 500),
+        new Platform({x: 7000, y: canvas.height-400}, 100, 20),
+        new Platform({x: 7400, y: canvas.height-300}, 100, 20),
+        new Platform({x: 7800, y: canvas.height-450}, 100, 20),
+        new Platform({x: 8200, y: canvas.height-350}, 2000, 600),];
+
+
+    let enemies = [
+        new enemy({x: 1400, y: canvas.height - 700}),
+        new enemy({x: 1500, y: canvas.height - 700}),
+        new enemy({x: 2700, y: canvas.height - 700}),
+        new enemy({x: 2800, y: canvas.height - 700}),
+        new enemy({x: 2900, y: canvas.height - 700}),
+        new enemy({x: 3500, y: canvas.height - 700}),
+        new enemy({x: 3800, y: canvas.height - 700}),
+        new enemy({x: 5100, y: canvas.height - 700}),
+        new enemy({x: 5600, y: canvas.height - 700}),
+        new enemy({x: 6000, y: canvas.height - 700}),
+        new enemy({x: 6400, y: canvas.height - 700}),
+        new enemy({x: 6800, y: canvas.height - 700}),
+        new enemy({x: 7200, y: canvas.height - 700}),
+    ];
+
+    const endLevel = new levelEnd({x: 10000, y: canvas.height - 350});
+
     const weapons = [new Weapon({x: player.position.x, y: player.position.y})];
     let projectiles = [];
-    const floor = new Floor({ y: canvas.height - 60, height: 60 });
-    platforms.push(floor);
-    let scrollOffSet = 0;
     const healthbar = new HealthBar(player.health);
+    const deathHoles = [
+        new DeathHole(0, 0),
+    ];
    
     
     animate();
@@ -428,7 +525,6 @@ function start() {
                 keys.right.pressed = true;
             break;
             case 87:
-                console.log(player.onGround);
                 if(player.onGround) {
                 player.velocity.y = -20;
                 player.onGround = false;
@@ -466,7 +562,7 @@ function start() {
         });
         projectiles.push(projectile);
     }
-}
+
    
     setInterval(() => {
         enemies.forEach(enemy => {
@@ -474,4 +570,4 @@ function start() {
             enemy.move(direction);
         });
     }, 1000);
-}
+}}
